db081318a66bdc392dab281448a578f4c6db8ac6
S.SKNS <somtida.th@gmail.com>
2016-10-20 23:58:27 -0700

    remove

--- src/js/components/Contact.js
+++ src/js/components/Contact.js
@@ -6,8 +6,8 @@ var Contact = React.createClass({
   handleEdit() {
     console.log('Edit');
   },
-  handleDelete() {
-    console.log('Delete');
+  handleRemove(i,j) {
+    console.log('Remove');
   },
   render(){
     return(
@@ -15,8 +15,8 @@ var Contact = React.createClass({
         <td>{this.props.contact.name}</td>
         <td>{this.props.contact.phone}</td>
         <td>{this.props.contact.email}</td>
-        <td><a href="#" className="btn btn-warning" onClick={this.handleEdit}>Edit</a></td>
-        <td><a href="#" className="btn btn-danger" onClick={this.handleDelete}>Delete</a></td>
+        <td><a href="#" className="btn btn-warning" onClick={this.handleEdit.bind(this))}>Edit</a></td>
+        <td><a href="#" className="btn btn-danger" onClick={this.handleRemove.bind(this, this.props.contact.id)}>Remove</a></td>
       </tr>
     )
   }
